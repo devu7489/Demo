156929f6fee5927916461556c5f4a375c141c6f7
MySociety2K15 <devendra.patel7489@gmail.com>
2021-05-07 21:29:47 +0530

    test 2

--- demo/react-ui/src/App.js
+++ demo/react-ui/src/App.js
@@ -14,6 +14,7 @@ class App extends Component{
 		// Test
 		// Adding test for testing rebase
 		// Test for pull
+		//Adding more test
 		const {loggedIn} = this.props;
 		return(
 			<div className="App">
